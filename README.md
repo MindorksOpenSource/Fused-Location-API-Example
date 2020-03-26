1673a26d192a91d08a82cd371172c55860f25bee
mishra3452 <mishrasumit.3452@gmail.com>
2020-03-26 11:32:53 +0530

    Implementation Done

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Fused Location API Example
+# Fused Location API Example - An example project to demonstrate how to use Fused Location API to fetch the current location in Android
+[![MindOrks](https://img.shields.io/badge/mindorks-opensource-blue.svg)](https://mindorks.com/open-source-projects)
+[![MindOrks Community](https://img.shields.io/badge/join-community-blue.svg)](https://mindorks.com/join-community)
+
+<p align="center">
+    <img src="https://github.com/MindorksOpenSource/Fused-Location-API-Example/assets/using-gps-location-manager-in-android-android-tutorial-banner.jpg">
+</p>
+<br>
+
+### Reference Blog Step by Step Guide - [Using Fused Location API To Fetch Current Location](https://blog.mindorks.com/using-gps-location-manager-in-android-android-tutorial)
--- app/src/main/java/com/mindorks/example/fusedlocation/MainActivity.kt
+++ app/src/main/java/com/mindorks/example/fusedlocation/MainActivity.kt
@@ -1,12 +1,96 @@
 package com.mindorks.example.fusedlocation
 
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.widget.Toast
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.mindorks.example.fusedlocation.utils.PermissionUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 999
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private fun setUpLocationListener() {
+        val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        // for getting the current location update after every 2 seconds with high accuracy
+        val locationRequest = LocationRequest().setInterval(2000).setFastestInterval(2000)
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+        fusedLocationProviderClient.requestLocationUpdates(
+                locationRequest,
+                object : LocationCallback() {
+                    override fun onLocationResult(locationResult: LocationResult) {
+                        super.onLocationResult(locationResult)
+                        for (location in locationResult.locations) {
+                            latTextView.text = location.latitude.toString()
+                            lngTextView.text = location.longitude.toString()
+                        }
+                        // Few more things we can do here:
+                        // For example: Update the location of user on server
+                    }
+                },
+                Looper.myLooper()
+        )
+    }
+
+    override fun onStart() {
+        super.onStart()
+        when {
+            PermissionUtils.isAccessFineLocationGranted(this) -> {
+                when {
+                    PermissionUtils.isLocationEnabled(this) -> {
+                        setUpLocationListener()
+                    }
+                    else -> {
+                        PermissionUtils.showGPSNotEnabledDialog(this)
+                    }
+                }
+            }
+            else -> {
+                PermissionUtils.requestAccessFineLocationPermission(
+                        this,
+                        LOCATION_PERMISSION_REQUEST_CODE
+                )
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    when {
+                        PermissionUtils.isLocationEnabled(this) -> {
+                            setUpLocationListener()
+                        }
+                        else -> {
+                            PermissionUtils.showGPSNotEnabledDialog(this)
+                        }
+                    }
+                } else {
+                    Toast.makeText(
+                            this,
+                            getString(R.string.location_permission_not_granted),
+                            Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
     }
 }
